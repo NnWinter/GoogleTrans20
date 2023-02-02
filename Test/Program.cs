dd7767b5c14e1c4f88f4d6beab7021c47a10038c
NnWinter <515621078@163.com>
2023-02-02 08:28:22 +0800

    测试清空文本成功

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using PuppeteerSharp;
+using static System.Net.Mime.MediaTypeNames;
 
 using var browserFetcher = new BrowserFetcher();
 await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
@@ -9,12 +10,35 @@ var browser = await Puppeteer.LaunchAsync(new LaunchOptions
 var page = await browser.NewPageAsync();
 await page.GoToAsync("https://fanyi.youdao.com/index.html");
 
-await page.Keyboard.SendCharacterAsync("啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
+var inpu = "div[id='js_fanyi_input']";
+
+// 尝试清除内容并重写
+
+
+string text1 = "哦哦哦哦哦哦哦哦哦哦~~~~~";
+string text2 = "啊啊啊啊啊啊啊啊啊啊啊！！！！";
+
+page.TypeAsync(inpu, text1).Wait();
 
 Thread.Sleep(2000);
 
 var content = await page.GetContentAsync();
+Console.WriteLine(content);
 
+
+// 尝试清空文本
+
+var input = await page.QuerySelectorAsync(inpu);
+await input.EvaluateFunctionAsync($"e => e.innerText = ' '");
+Thread.Sleep(100);
+
+//
+
+
+page.TypeAsync(inpu, text2).Wait();
+
+Thread.Sleep(2000);
+content = await page.GetContentAsync();
 Console.WriteLine(content);
 
 Console.ReadLine();
